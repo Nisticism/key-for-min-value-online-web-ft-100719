19aeda95c2c539e7d8845ab5037a7b80ec477d30
Hans Foster <fosterhans@gmail.com>
2019-10-30 01:43:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,12 @@ def key_for_min_value(name_hash)
   end
   min_value = name_hash[0]
   min_key = 0
+  all_values = []
   name_hash.each do |name_key, name_value|
-    if name_value < min_value
-      min_key = name_key
-    end
+    all_values << name_value
   end
+  min_value = all_values[0]
+  all_values.each do |value|
+    if value 
   min_key
 end
